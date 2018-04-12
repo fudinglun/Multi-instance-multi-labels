99e49340ea6ce82812c1549dc65e077d0106f0f4
DylanFu <fudinglun94@berkeley.edu>
2018-04-12 11:25:18 -0400

    deal with multi process

--- average_sbatch.sh
+++ average_sbatch.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem=5GB
 #SBATCH --job-name=Yelp
 #SBATCH --mail-type=END,FAIL
